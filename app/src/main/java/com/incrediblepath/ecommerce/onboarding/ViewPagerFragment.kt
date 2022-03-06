c77d5a5dee8097329733b482100122a82304b117
lavanya240994 <lavanya.ga94@gmail.com>
2022-03-05 21:22:22 -0500

    Added new images and implemented dots.

--- app/src/main/java/com/incrediblepath/ecommerce/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/incrediblepath/ecommerce/onboarding/ViewPagerFragment.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import androidx.viewpager2.widget.ViewPager2
 import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayoutMediator
 import com.incrediblepath.ecommerce.R
 import com.incrediblepath.ecommerce.onboarding.adapters.ViewPagerAdapter
 
@@ -17,52 +17,61 @@ import com.incrediblepath.ecommerce.onboarding.fragments.CommonFragment
 
 class ViewPagerFragment : Fragment() {
     private lateinit var fragmentViewPagerBinding: FragmentViewPagerBinding
+    private lateinit var list: ArrayList<Fragment>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        list = arrayListOf(CommonFragment(0), CommonFragment(1), CommonFragment(2),)
         fragmentViewPagerBinding = FragmentViewPagerBinding.inflate(inflater, container, false)
         fragmentViewPagerBinding.viewPager.adapter = ViewPagerAdapter(
             requireActivity().supportFragmentManager,
             lifecycle,
-            arrayListOf(CommonFragment(),CommonFragment(),CommonFragment())
+            list
         )
 
         fragmentViewPagerBinding.leftNav.setOnClickListener {
             val currentItem = fragmentViewPagerBinding.viewPager.currentItem
-            if(currentItem>0) {
+            if (currentItem > 0) {
                 fragmentViewPagerBinding.viewPager.setCurrentItem(currentItem - 1)
             }
         }
         fragmentViewPagerBinding.rightNav.setOnClickListener {
             val currentItem = fragmentViewPagerBinding.viewPager.currentItem
-            if(currentItem<2) {
+            if (currentItem < list.size-1) {
                 fragmentViewPagerBinding.viewPager.setCurrentItem(currentItem + 1)
             }
         }
 
-        fragmentViewPagerBinding.viewPager.registerOnPageChangeCallback(object :OnPageChangeCallback(){
+        fragmentViewPagerBinding.viewPager.registerOnPageChangeCallback(object :
+            OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
-                if(position==0){
+                if (position == 0) {
                     fragmentViewPagerBinding.leftNav.visibility = View.GONE
                     fragmentViewPagerBinding.rightNav.visibility = View.VISIBLE
                     fragmentViewPagerBinding.finish.visibility = View.GONE
-                }else if(position == 2){
+                } else if (position == list.size-1) {
                     fragmentViewPagerBinding.rightNav.visibility = View.GONE
                     fragmentViewPagerBinding.leftNav.visibility = View.VISIBLE
                     fragmentViewPagerBinding.finish.visibility = View.VISIBLE
-                }else{
+                } else {
                     fragmentViewPagerBinding.leftNav.visibility = View.VISIBLE
                     fragmentViewPagerBinding.rightNav.visibility = View.VISIBLE
                     fragmentViewPagerBinding.finish.visibility = View.GONE
                 }
             }
+
+
         })
 
-        fragmentViewPagerBinding.finish.setOnClickListener{
+        fragmentViewPagerBinding.finish.setOnClickListener {
             findNavController().navigate(R.id.action_viewPagerFragment_to_loginFragment)
         }
 
+        TabLayoutMediator(fragmentViewPagerBinding.tabLayout, fragmentViewPagerBinding.viewPager) { tab, position ->
+            //tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
         return fragmentViewPagerBinding.root
     }
 }
--- app/src/main/java/com/incrediblepath/ecommerce/onboarding/adapters/ViewPagerAdapter.kt
+++ app/src/main/java/com/incrediblepath/ecommerce/onboarding/adapters/ViewPagerAdapter.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class ViewPagerAdapter(fragmentManager: FragmentManager,lifecycle: Lifecycle,private val list:ArrayList<Fragment>):FragmentStateAdapter(fragmentManager,lifecycle) {
+class ViewPagerAdapter(
+    fragmentManager: FragmentManager,
+    lifecycle: Lifecycle,
+    private val list: ArrayList<Fragment>
+) : FragmentStateAdapter(fragmentManager, lifecycle) {
     override fun getItemCount(): Int {
-       return list.size
+        return list.size
     }
 
     override fun createFragment(position: Int): Fragment {
